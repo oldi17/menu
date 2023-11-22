0ffcfdeda353f333f0d8bf2ff4257d9ecae7341b
oldi <odi.scs.15@gmail.com>
2023-11-22 19:51:58 +0300

    minor cleanup

--- menu/admin.py
+++ menu/admin.py
@@ -1,27 +1,5 @@
 from django.contrib import admin
-from django.forms import ModelForm
 from .models import Menu, Item
-
-# @admin.register(Item)
-# class MenuItemAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'parent',)
-#     list_filter = ('menu__title',)
-#     # fieldsets = [
-#     #     ( None, 
-#     #         {
-#     #             'fields': (
-#     #                 'menu', 
-#     #                 'parent', 
-#     #                 'title',
-#     #                 )
-#     #         }
-#     #     ),
-#     # ]
-
-#     def get_form(self, request, obj=None, **kwargs):
-#         if obj: # obj is not None, so this is a change page
-#             kwargs['exclude'] = ['menu', ]
-#         return super(MenuItemAdmin, self).get_form(request, obj, **kwargs)
 
 class MenuItemInline(admin.TabularInline):
     model = Item
